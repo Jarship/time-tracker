490a0d7db3e8b8ab83cccf4bedeaee9181b4d94f
Jarship <JohnArthurBishop@gmail.com>
2019-11-14 19:47:52 -0500

    Updates

---
+++ precache-manifest.71d20bb09b0b8046b42959dbf4f878b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b3b7a03838e2a966517587caf14717",
+    "url": "/Time-Tracker/index.html"
+  },
+  {
+    "revision": "825a3f3a602bec90a084",
+    "url": "/Time-Tracker/static/css/main.8d614f59.chunk.css"
+  },
+  {
+    "revision": "5096640427b435e61827",
+    "url": "/Time-Tracker/static/js/2.ac9abcb5.chunk.js"
+  },
+  {
+    "revision": "825a3f3a602bec90a084",
+    "url": "/Time-Tracker/static/js/main.cef6a823.chunk.js"
+  },
+  {
+    "revision": "d27c9cd870fe71da9453",
+    "url": "/Time-Tracker/static/js/runtime-main.44d50a80.js"
+  }
+]);
